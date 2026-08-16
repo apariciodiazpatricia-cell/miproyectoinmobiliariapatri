7f9cea9f2deb50de20249b07cbadf81aadd65295
apariciodiazpatricia-cell <apariciodiazpatricia@gmail.com>
2026-08-16 21:47:06 +0200

    fix(booking): anadir importacion de Link en BookingMain para evitar ReferenceError

--- src/components/sections/BookingMain.jsx
+++ src/components/sections/BookingMain.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { featuredProperties } from '../../data/properties'
 import { bookingSteps } from '../../data/bookingSteps'
 
